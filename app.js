61f2f2582e9a24809b02838f59f9117fec0e7ec2
unknown <naimul8080@gmail.com>
2021-08-21 12:58:25 +0600

    added new coded

--- app.js
+++ app.js
@@ -1,32 +1,91 @@
-// function getInputMemory(memory, add) {
-//     const inputMemory = document.getElementById(memory);
-//     const inputMemoryText = inputMemory.innerText;
-
-//     if (add == true) {
-//         inputMemory.innerText = 000;
-//     }
-//     else if (add == false) {
-//         inputMemory.innerText == 000;
-//         console.log(inputMemory)
-//     }
-// }
-
-
-
-document.getElementById('8gb-memory').addEventListener('click', function () {
-    const memory16Gb = document.getElementById('memory');
-    memory16Gb.innerText = 00;
-    // getInputMemory('memory', true)
-})
-document.getElementById('16gb-memory').addEventListener('click', function () {
-    const memory16Gb = document.getElementById('memory');
-    const memory16GbText = memory16Gb.innerText
-    memory16Gb.innerText = 180;
+//this function for memory
+function getInputMemory(memory, isIncrease) {
+    const inputMemory = document.getElementById('memory');
+    inputMemory.innerText = memory;
     const totalPrice = document.getElementById('total-price');
-
     const totalPriceText = totalPrice.innerText
-    // totalPrice.innerText = parseInt(totalPriceText) + 180;
-    totalPrice.innerText = 1299 + 180;
+    if (isIncrease == true) {
+        totalPrice.innerText = 1299;
+    }
+    else if (isIncrease == false) {
+        totalPrice.innerText = 1479;
+    }
+}
+//8gb memory button
+document.getElementById('8gb-memory').addEventListener('click', function () {
+    getInputMemory(0, true)
+})
+//16gb memory button
+document.getElementById('16gb-memory').addEventListener('click', function () {
+    getInputMemory(180, false)
 
-    // getInputMemory('memory', false)
+})
+
+//this function for storage 
+function getInputStorage(storage, price, isIncrease) {
+    const inputStorage = document.getElementById(storage);
+    inputStorage.innerText = price;
+    const totalPrice = document.getElementById('total-price');
+    const totalPriceText = totalPrice.innerText
+    if (isIncrease == true && price == 0) {
+        totalPrice.innerText = 1299;
+    }
+    else if (isIncrease == false && price == 100) {
+        totalPrice.innerText = 1399;
+    }
+    else if (isIncrease == false && price == 180) {
+        totalPrice.innerText = 1479;
+    }
+
+}
+
+//256gb ssd card
+document.getElementById('256gb-storage').addEventListener('click', function () {
+    getInputStorage('storage', 0, true)
+})
+//512gb ssd card
+document.getElementById('512gb-storage').addEventListener('click', function () {
+    getInputStorage('storage', 100, false)
+})
+//1tb ssd card
+document.getElementById('1tb-storage').addEventListener('click', function () {
+    getInputStorage('storage', 180, false)
+})
+
+//this function for delivery charge
+function getInputDelivery(charge, isIncrease) {
+    const inputMemory = document.getElementById('delivery');
+    inputMemory.innerText = charge;
+    const totalPrice = document.getElementById('total-price');
+    // const totalPriceText = totalPrice.innerText
+    if (isIncrease == true) {
+        totalPrice.innerText = 1299;
+    }
+    else if (isIncrease == false) {
+        totalPrice.innerText = 1319;
+    }
+}
+
+//free delivery
+document.getElementById('free-delivery').addEventListener('click', function () {
+    getInputDelivery(0, true)
+})
+//pay delivery
+document.getElementById('pay-delivery').addEventListener('click', function () {
+    getInputDelivery(20, false)
+
+})
+
+//coupon code 
+document.getElementById('apply-button').addEventListener('click', function () {
+    const totalPrice = document.getElementById('total-price');
+    const totalPriceText = totalPrice.innerText;
+    // const totalPriceAmount = parseInt(totalPriceText)
+    const total = document.getElementById('total');
+    const discount = (totalPriceText * 20) / 100;
+    const couponCode = document.getElementById('coupon-code')
+    const couponCodeValue = couponCode.value;
+    if (couponCodeValue == 'stevekaku') {
+        total.innerText = totalPriceText - discount
+    }
 })
